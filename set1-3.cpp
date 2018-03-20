97ed6bc07380998779bd3623b2fbd4063f25a430
Meenalosini <37570651+Meenalosini@users.noreply.github.com>
2018-03-20 21:46:50 +0530

    Create set1-3.cpp

---
+++ set1-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+	{
+		cout<<"vowel";
+	}
+	else
+	{
+		cout<<"consonant";
+	}
+		
+	return 0;
+}
