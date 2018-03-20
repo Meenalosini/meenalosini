c9dfc91a333485ae3ef90f8d4448ea6667ea1df8
Meenalosini <37570651+Meenalosini@users.noreply.github.com>
2018-03-20 17:37:15 +0530

    Create set1-1.cpp

---
+++ set1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >>n;
+	if(n<0)
+	{
+		cout<<"n is negative";
+	}
+	else if(n>0)
+	{
+		cout<<"n is positive";
+	}
+	else
+	{
+		cout<<"n is zero";
+	}
+
+	return 0;
+}
