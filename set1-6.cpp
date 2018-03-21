78ff24bbeaa88a5173b7163441757fdf45e64701
Meenalosini <37570651+Meenalosini@users.noreply.github.com>
2018-03-21 11:01:17 +0530

    Create set1-6.cpp

---
+++ set1-6.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+ int year;
+ cin>>year;
+ cout<<"Enter a year";
+ if(year%4==0)
+ {
+    cout<<" is a leap year"; 
+ }
+ else
+ {
+     cout<<" is not a leap year";
+ }
+
+    return 0;
+}
