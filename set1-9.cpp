dbc49054ed4476ff945447ade6ba09ae0ab11dfa
Meenalosini <37570651+Meenalosini@users.noreply.github.com>
2018-04-19 20:48:37 +0530

    Create set1-9.cpp

---
+++ set1-9.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,k,sum=0,a[100];
+    cin>> n,k;
+    cout<<"enter two numbers";
+    for(int i=1;i<n;i++)
+    {
+        cout<<i;
+    }
+    for(int i=1;i<k;i++)
+    {
+    sum=sum+i;
+    cout<<"sum is"<< sum<<endl;
+        
+    }
+
+
+    return 0;
+}
